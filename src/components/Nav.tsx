e572f69c044f92d74b1fba8a537e9886625379b5
AK2000YY <abdyabkor@gmail.com>
2025-07-23 02:23:47 +0300

    add loading to logout

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,22 +1,27 @@
 import supabase from "@/lib/supabase"
 import { Button } from "./ui/button"
 import { toast } from "sonner"
-import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "./ui/alert-dialog"
-import type { ReactNode } from "react"
+import { AlertDialog, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "./ui/alert-dialog"
+import { useState, type ReactNode } from "react"
+import FormButton from "./FormButton"
+import { LoaderCircle } from "lucide-react"
 
 const Nav = ({children} : {
     children?: ReactNode
 }) => {
 
+  const [open, setOpen] = useState(false)
+
   const handleLogout = async () => {
     const { error } = await supabase.auth.signOut()
     if(error)
         toast.error('شيء ما خاطئ!')
+    setOpen(false)
   }
 
   return (
     <div className="w-screen flex justify-between p-3">
-        <AlertDialog>
+        <AlertDialog open={open} onOpenChange={setOpen}>
             <AlertDialogTrigger asChild>
                 <Button 
                 className="bg-red-600"
@@ -35,11 +40,13 @@ const Nav = ({children} : {
                 </AlertDialogHeader>
                 <AlertDialogFooter>
                     <AlertDialogCancel>إلغاء</AlertDialogCancel>
-                    <AlertDialogAction
-                        onClick={handleLogout}
-                    >
-                        نعم
-                    </AlertDialogAction>
+                    <form action={handleLogout}>
+                        <FormButton
+                            name="نعم"
+                        >
+                            <LoaderCircle className="animate-spin text-[#988561]" />
+                        </FormButton>
+                    </form>
                 </AlertDialogFooter>
             </AlertDialogContent>
         </AlertDialog>
