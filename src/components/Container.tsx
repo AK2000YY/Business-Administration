8c1013befe92c3e38acc32b85b2574237918f281
AK2000YY <abdyabkor@gmail.com>
2025-07-22 04:28:04 +0300

    create container

---
+++ src/components/Container.tsx
@@ -0,0 +1,17 @@
+import logo from "../assets/logo.svg"
+import type { ReactNode } from "react"
+
+const Container = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="w-screen h-screen relative">
+      <div className="absolute top-0 left-0 z-0 flex w-screen h-screen justify-center items-center">
+        <img src={logo} className="size-200 object-cover" />
+      </div>
+      <div className="absolute top-0 right-0 w-full h-full z-10">
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default Container
