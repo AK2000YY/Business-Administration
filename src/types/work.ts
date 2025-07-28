2c7447919dcc678190bd8168314dffafba100a6b
AK2000YY <abdyabkor@gmail.com>
2025-07-28 03:58:09 +0300

    edit work type

--- src/types/work.ts
+++ src/types/work.ts
@@ -1,15 +1,19 @@
-import type { DeviceType } from "./device_type"
-import type { Service } from "./service";
+import type { DeviceType } from "./device_type";
 
 type Work = {
   id: string;
-  device_name: string;
   device_types: DeviceType;
-  owning_entity: string;
-  notes: string;
-  services: Service[];
   serial: string;
+  attachment?: string;
+  recieve_data?: string;
+  wifi_card?: string;
+  status?: "انتظار" | "مكتمل" | "ملغى";
+  brother_name?: string;
+  entity?: string;
+  contact_number?: string;
+  ram?: number;
+  cpu?: string;
+  model?: string;
 };
 
-
-export { type Work }
+export { type Work };
