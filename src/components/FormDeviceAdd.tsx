728794320967a21faecb56bae9f2622c651039a5
AK2000YY <abdyabkor@gmail.com>
2025-08-01 02:46:40 +0300

    refactor form device add

--- src/components/FormDeviceAdd.tsx
+++ src/components/FormDeviceAdd.tsx
@@ -1,51 +1,278 @@
-import { DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from './ui/dialog'
-import { Label } from './ui/label'
-import { Input } from './ui/input'
-import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from './ui/select'
-import type { DeviceType } from '@/types/device_type'
-import { Button } from './ui/button'
+import {
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "./ui/dialog";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import type { DeviceType } from "@/types/device_type";
+import { Button } from "./ui/button";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { ChevronDownIcon } from "lucide-react";
+import { Calendar } from "./ui/calendar";
+import { useEffect, useState } from "react";
+import type { Cpu } from "@/types/cpu";
+import supabase from "@/lib/supabase";
+import { toast } from "sonner";
+import { ramType } from "@/types/ram_type";
+import { deviceStatus } from "@/types/device_status";
+import { wifiCardStatus } from "@/types/wifi_card_status";
+import FileComponent from "./FileComponent";
 
-const FormDeviceAdd = ({types, handleAdd} : {
-  types: DeviceType[],
-  handleAdd: (e: React.FormEvent<HTMLFormElement>) => void
+const FormDeviceAdd = ({
+  types,
+  arrivalOpen,
+  setArrivalOpen,
+  arrivalDate,
+  setArrivalDate,
+  handleAdd,
+}: {
+  types: DeviceType[];
+  arrivalOpen: boolean;
+  arrivalDate: Date | undefined;
+  setArrivalOpen: any;
+  setArrivalDate: any;
+  handleAdd: (e: React.FormEvent<HTMLFormElement>) => void;
 }) => {
+  const [cpus, setCpus] = useState<Cpu[]>([]);
 
+  useEffect(() => {
+    const getCpus = async () => {
+      const { data, error } = await supabase.from("cpus").select("*");
+      console.log(data);
+      if (error) toast.error("حدث خطأ ما!");
+      else setCpus(data ?? []);
+    };
+    getCpus();
+  }, []);
 
   return (
-    <DialogContent>
+    <DialogContent className="min-w-fit">
       <form onSubmit={handleAdd}>
         <DialogHeader>
           <DialogTitle>إضافة جهاز جديد</DialogTitle>
-          <DialogDescription>إملأ الحقول المطلوبة لإضافة جهاز جديد</DialogDescription>
+          <DialogDescription>
+            إملأ الحقول المطلوبة لإضافة جهاز جديد
+          </DialogDescription>
         </DialogHeader>
         <div className="grid gap-4 py-2">
           <div className="flex gap-x-2">
-            <Label htmlFor="serial" className="w-30">الرقم التسلسلي</Label>
-            <Input id="serial" name="serial" placeholder="ادخل رقم الجهاز التسلسلي" />
+            <Label htmlFor="serial" className="min-w-22">
+              الرقم التسلسلي
+            </Label>
+            <Input
+              id="serial"
+              name="serial"
+              placeholder="ادخل رقم الجهاز التسلسلي"
+            />
           </div>
           <div className="flex gap-x-2">
-            <Label htmlFor="device_name" className="w-30">اسم الجهاز</Label>
-            <Input id="device_name" name="device_name" placeholder="ادخل اسم الجهاز" />
+            <Label htmlFor="company" className="min-w-22">
+              الشركة المصنعة
+            </Label>
+            <Input
+              id="company"
+              name="company"
+              placeholder="ادخل اسم الشركة المصنعة"
+            />
           </div>
           <div className="flex gap-x-2">
-            <Label className="max-w-30">النوع</Label>
+            <Label className="min-w-22">النوع</Label>
             <Select name="device_type_id">
-              <SelectTrigger className="w-[180px]">
+              <SelectTrigger className="w-[180px] ">
                 <SelectValue placeholder="اختر النوع" />
               </SelectTrigger>
               <SelectContent>
                 <SelectGroup>
                   <SelectLabel>الأنواع</SelectLabel>
-                  {types.map(ele => 
-                    <SelectItem key={ele.id} value={ele.id}>{ele.type}</SelectItem>
-                  )}
+                  {types.map((ele) => (
+                    <SelectItem key={ele.id} value={ele.id}>
+                      {ele.type}
+                    </SelectItem>
+                  ))}
                 </SelectGroup>
               </SelectContent>
             </Select>
           </div>
           <div className="flex gap-x-2">
-            <Label htmlFor="owning_entity" className="w-30">الجهة المالكة</Label>
-            <Input id="owning_entity" name="owning_entity" placeholder="ادخل الجهة المالكة" />
+            <Label htmlFor="model" className="min-w-22">
+              الموديل
+            </Label>
+            <Input id="model" name="model" placeholder="الموديل" />
+          </div>
+          <div className="flex gap-x-2">
+            <div className="flex gap-x-2">
+              <Label htmlFor="cpu" className="text-nowrap min-w-22">
+                اسم المعالج
+              </Label>
+              <Select name="cpu_id">
+                <SelectTrigger className="w-[180px] ">
+                  <SelectValue placeholder="اختر المعالج" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>المعالجات</SelectLabel>
+                    {cpus.map((ele) => (
+                      <SelectItem key={ele.id} value={ele.id}>
+                        {ele.name}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+            <div className="flex gap-x-2">
+              <Label htmlFor="cpu_name" className="text-nowrap  min-w-22">
+                جيل المعالج
+              </Label>
+              <Input
+                id="cpu_name"
+                name="cpu_name"
+                placeholder="ادخل اسم المعالج"
+              />
+            </div>
+          </div>
+          <div className="flex gap-x-2">
+            <div className="flex gap-x-2">
+              <Label className="text-nowrap min-w-22">نوع الرام</Label>
+              <Select name="ram_type">
+                <SelectTrigger className="w-[180px] ">
+                  <SelectValue placeholder="اختر نوع الرام" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>أنواع الرام</SelectLabel>
+                    {ramType.map((ele) => (
+                      <SelectItem key={ele} value={ele}>
+                        {ele}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+            <div className="flex gap-x-2">
+              <Label htmlFor="ram" className="text-nowrap  min-w-22">
+                حجم الرام
+              </Label>
+              <Input
+                id="ram"
+                name="ram"
+                type="number"
+                placeholder="ادخل حجم الرام"
+              />
+            </div>
+          </div>
+          <div className="flex gap-x-2">
+            <Label className="min-w-22">تاريخ الوصول</Label>
+            <Popover open={arrivalOpen} onOpenChange={setArrivalOpen}>
+              <PopoverTrigger asChild>
+                <Button
+                  variant="outline"
+                  id="date"
+                  className="w-48 justify-between font-normal"
+                >
+                  {arrivalDate
+                    ? arrivalDate.toLocaleDateString()
+                    : "اختر التاريخ"}
+                  <ChevronDownIcon />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent
+                className="w-auto overflow-hidden p-0"
+                align="start"
+              >
+                <Calendar
+                  mode="single"
+                  selected={arrivalDate}
+                  captionLayout="dropdown"
+                  onSelect={(date) => {
+                    setArrivalDate(date);
+                    setArrivalOpen(false);
+                  }}
+                />
+              </PopoverContent>
+            </Popover>
+          </div>
+          <div className="flex gap-x-2">
+            <Label className="min-w-22">الحالة</Label>
+            <Select name="status">
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="اختر الحالة" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>الحالة</SelectLabel>
+                  {deviceStatus.map((ele) => (
+                    <SelectItem key={ele} value={ele}>
+                      {ele}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="flex gap-x-2">
+            <Label htmlFor="brother_name" className="min-w-22">
+              اسم الأخ
+            </Label>
+            <Input
+              id="brother_name"
+              name="brother_name"
+              placeholder="ادخل اسم الأخ"
+            />
+          </div>
+          <div className="flex gap-x-2">
+            <Label htmlFor="entity" className="min-w-22">
+              الجهة
+            </Label>
+            <Input id="entity" name="entity" placeholder="ادخل الجهة" />
+          </div>
+          <div className="flex gap-x-2">
+            <Label htmlFor="contact_number" className="min-w-22">
+              رقم التواصل
+            </Label>
+            <Input
+              id="contact_number"
+              name="contact_number"
+              placeholder="ادخل رقم التواصل"
+            />
+          </div>
+          <div className="flex gap-x-2">
+            <Label htmlFor="attachment" className="min-w-22">
+              المرفق
+            </Label>
+            <FileComponent id="attachment" name="attachment" />
+          </div>
+          <div className="flex gap-x-2">
+            <Label className="w-22">كرت الشبكة</Label>
+            <Select name="wifi_card">
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="اختر ان كان موجود ام لا" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>كرت الشبكة</SelectLabel>
+                  {wifiCardStatus.map((ele) => (
+                    <SelectItem key={ele} value={ele}>
+                      {ele}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
           </div>
         </div>
         <DialogFooter>
@@ -56,7 +283,7 @@ const FormDeviceAdd = ({types, handleAdd} : {
         </DialogFooter>
       </form>
     </DialogContent>
-  )
-}
+  );
+};
 
-export default FormDeviceAdd
+export default FormDeviceAdd;
