79c5c0d5fbf870aefa810205cc071edc9f301543
AK2000YY <abdyabkor@gmail.com>
2025-08-04 00:06:05 +0300

    remove one card

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -12,7 +12,6 @@ import { ChevronLeft } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import devices from "../assets/devices.jpg";
 import dataEntry from "../assets/data_entry.jpeg";
-import cpuImage from "../assets/cpu.png";
 import passwordImage from "../assets/password.png";
 
 const Main = () => {
@@ -21,15 +20,15 @@ const Main = () => {
     <div className="w-screen h-screen flex flex-col">
       <Nav />
       <div className="w-screen flex flex-wrap grow justify-around items-start gap-y-2 overflow-y-auto">
-        <Card className="w-[45%] bg-transparent">
+        <Card
+          className="w-[35%] bg-transparent"
+          onClick={() => navigate("/types")}
+        >
           <CardHeader>
             <CardTitle>انواع الاجهزة</CardTitle>
             <CardDescription>تصفح انواع الأجهزة</CardDescription>
             <CardAction>
-              <Button
-                className="size-10 rounded-full"
-                onClick={() => navigate("/types")}
-              >
+              <Button className="size-10 rounded-full">
                 <ChevronLeft strokeWidth={4} />
               </Button>
             </CardAction>
@@ -38,15 +37,15 @@ const Main = () => {
             <img className="rounded-2xl opacity-90 h-80 w-full" src={devices} />
           </CardContent>
         </Card>
-        <Card className="w-[45%] bg-transparent">
+        <Card
+          className="w-[35%] bg-transparent"
+          onClick={() => navigate("/works")}
+        >
           <CardHeader>
             <CardTitle>أرشيف الأجهزة</CardTitle>
             <CardDescription>تصفح الأجهزة</CardDescription>
             <CardAction>
-              <Button
-                className="size-10 rounded-full"
-                onClick={() => navigate("/works")}
-              >
+              <Button className="size-10 rounded-full">
                 <ChevronLeft strokeWidth={4} />
               </Button>
             </CardAction>
@@ -58,35 +57,15 @@ const Main = () => {
             />
           </CardContent>
         </Card>
-        <Card className="w-[45%] bg-transparent">
-          <CardHeader>
-            <CardTitle>المعالجات</CardTitle>
-            <CardDescription>تصفح اسماء المعالجات</CardDescription>
-            <CardAction>
-              <Button
-                className="size-10 rounded-full"
-                onClick={() => navigate("/cpus")}
-              >
-                <ChevronLeft strokeWidth={4} />
-              </Button>
-            </CardAction>
-          </CardHeader>
-          <CardContent>
-            <img
-              className="rounded-2xl opacity-90 h-80 w-full"
-              src={cpuImage}
-            />
-          </CardContent>
-        </Card>
-        <Card className="w-[45%] bg-transparent">
+        <Card
+          className="w-[35%] bg-transparent"
+          onClick={() => navigate("/passwords")}
+        >
           <CardHeader>
             <CardTitle>كلمات السر</CardTitle>
             <CardDescription>تصفح كلمات السر</CardDescription>
             <CardAction>
-              <Button
-                className="size-10 rounded-full"
-                onClick={() => navigate("/passwords")}
-              >
+              <Button className="size-10 rounded-full">
                 <ChevronLeft strokeWidth={4} />
               </Button>
             </CardAction>
@@ -98,6 +77,7 @@ const Main = () => {
             />
           </CardContent>
         </Card>
+        <Card className="w-[35%] bg-transparent opacity-0"></Card>
       </div>
     </div>
   );
